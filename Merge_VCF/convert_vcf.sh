8bd73c66a8a83937e5fd29f413eeb800bf2df1ce
Kenny Cheng <kenny77527@gmail.com>
2022-08-15 21:17:21 +0800

    updated

--- Merge_VCF/convert_vcf.sh
+++ Merge_VCF/convert_vcf.sh
@@ -1,5 +1,6 @@
 Input_dir='/staging2/reserve/flagship/u3121714/kenny/Input/BaseData/hg38/HBOC_200/VCF/'
 tool='/staging2/reserve/flagship/u3121714/kenny/Aging_script/Team1/Script/tool/bin/vcftools_v0.1.16/bin/'
+filename='HBOC_base.list'
 
 ## Check for dir, if not found create it using the mkdir ##
 convert_dir='/staging2/reserve/flagship/u3121714/kenny/Input/BaseData/hg38/HBOC_200/convert_vcf/'
@@ -14,5 +15,5 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	# Remove indels
 	${tool}vcftools --vcf "${Input_dir}${line}".vcf --remove-indels --recode --recode-INFO-all --out ${Input_dir}${line}
   # Convert VCF4.1 to VCF4.2
-  cat "${Input_dir}${line}".recode.vcf | ${tool}vcftools_v0.1.16/bin/vcf-convert -v 4.2 > ${convert_dir}${line}
-done < $filename
+  cat "${Input_dir}${line}".recode.vcf | ${tool}vcf-convert -v 4.2 > ${convert_dir}${line}
+done < ${Input_dir}${filename}
