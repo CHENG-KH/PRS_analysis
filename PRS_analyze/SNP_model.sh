bf1bcf2ed70f8752efff35c90f62e519654dc45a
Kenny Cheng <kenny77527@gmail.com>
2022-09-16 11:03:10 +0800

    updated

--- PRS_analyze/SNP_model.sh
+++ PRS_analyze/SNP_model.sh
@@ -1,7 +1,10 @@
 
-
-
-
+output_basedata_pathway='/staging2/reserve/flagship/u3121714/kenny/Output/Basedata/'
+phenoarray=HBOC_110
+# --------------------------------------------------
+cd ${output_basedata_pathway}$phenoarray
+# Remove effect size = NA
 awk '$11 !~ /NA/' finalBase.txt > finalBase_noNA.txt
 
+# Filter p <= 0.01, keep CHR, POS, A1, A2, P, EFFECT_SIZE
 awk '$9 <= 0.01 {printf $1"\t"$3"\t"$9"\t"$11"\n"}' finalBase_noNA.txt > SNP_p_0.01.txt
